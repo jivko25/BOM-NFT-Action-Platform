fd7fcad9a5ad89ace5d01933c774daad60940331
jivko25 <jivkou@yahoo.com>
2021-10-21 19:42:52 +0300

    add and test Logo component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,12 @@
 import Example from "../src/components/example/Example";
+import Logo from "../src/components/Logo/Logo";
 
 export default function Index() {
-  return <Example />;
+  return (
+    //Test with if both variants are working and they are working :)
+    <div>
+      <Logo type="muted"/>
+      <Logo/>
+    </div>
+  );
 }
