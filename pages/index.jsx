3fc654fef7d6e2a05b963745025b657223e02be3
jivko25 <jivkou@yahoo.com>
2021-10-22 00:57:27 +0300

    add Header component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 import Example from "../src/components/example/Example";
 import Logo from "../src/components/logo/Logo";
 import Avatar from "../src/components/avatar/Avatar";
+import Header  from "../src/components/header/Header";
 
 export default function Index() {
   return (
     <div>
-      <Logo type="muted"/>
+      <Header/>
       <Avatar url = '/images/avatar.png' verified = "true"/>
     </div>
   );
