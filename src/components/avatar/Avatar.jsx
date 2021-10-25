260bd1c0e1b5671a8af9e10f7718e9ca0dadb349
jivko25 <jivkou@yahoo.com>
2021-10-26 01:37:58 +0300

    fix: img self closing tag and remove img alt

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,8 +3,8 @@ import styles from './Avatar.module.scss';
 function Avatar({size = 90, url, verified = false}){
     return(
         <div style={{width: size, height: size}} className={styles.avatar}>
-            <img src = {url} alt = "avatar" className={styles.image}></img>
-            {verified ? <img src = '/images/verified.svg' className={styles.badge}></img> : null}
+            <img src = {url} className={styles.image}/>
+            {verified ? <img src = '/images/verified.svg' className={styles.badge}/> : null}
         </div>
     )
 }
