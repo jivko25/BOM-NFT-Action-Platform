8ed8416a7287267778b9ca2003bdad733758e9c1
jivko25 <jivkou@yahoo.com>
2021-10-31 00:52:05 +0300

    add Trending component

---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import Container from "@mui/material/Container";
+import styles from "./Trending.module.scss";
+import classNames from "classnames";
+import Card from "../card/Card"
+import Select from '@mui/material/Select';
+import { Avatar, Grid } from "@mui/material";
+import { MenuItem } from "@mui/material";
+
+const options = ["Today", "This week", "This month"];
+
+export default function Trending({cards = []}){
+        const [timeOption, setTimeOption] = useState(options[0]);
+
+        return (
+            <Container className={classNames(styles.container)} maxWidth="xl">
+                <Grid container spacing={12}>
+                    <Grid item xs={10}>
+                        <h1>Trending</h1>
+                    </Grid >
+                    <Grid item xs={2}>
+                      <Select className={classNames(styles.select)} label="Age"
+                      value={timeOption}
+                      onChange={(event) => {
+                        setTimeOption(event.target.value)
+                      }}>
+                          {options.map(option => {
+                                return <MenuItem key = {option} value={option}>{option}</MenuItem>
+                          })
+                          }
+                      </Select>
+                    </Grid >
+                </Grid>
+               
+                <Grid container spacing={3}>
+                    {(
+                        cards.map(card => {
+                            return   <Grid item xs={3}>
+                                        <Card {...card} />
+                                    </Grid >
+                    }))}
+                </Grid>
+            </Container>
+          );
+}
