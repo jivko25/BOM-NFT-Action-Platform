3a42784c2a7ec929f920162cdf8dd31d0fbd8f2e
jivko25 <jivkou@yahoo.com>
2021-11-07 00:47:13 +0200

    add TopCollectors component

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,34 +12,86 @@ import How from "../src/components/how/How";
 import Featured from "../src/components/featured/Featured";
 import Collector from "../src/components/collectors/Collector";
 import CollectorColumn from "../src/components/collectors/CollectorColumn";
+import TopCollectors from "../src/components/collectors/TopCollectors";
 
 export default function Index() {
   return (
     <div>
       <Header/>
-      <CollectorColumn items = {[
-            {
-              id: 1,
-              name: "Peter",
-              nftsCount: 12312,
-              avatar: "/images/avatar.png",
-              verified: true,
-            },
-            {
-              id: 2,
-              name: "John",
-              nftsCount: 1111,
-              avatar: "/images/avatar.png",
-              verified: true,
-            },
-            {
-              id: 3,
-              name: "Steven",
-              nftsCount: 432,
-              avatar: "/images/avatar.png",
-              verified: true,
-            }
-          ]}/>
+      <TopCollectors collectors = {[
+    {
+      name: "Peter",
+      nftsCount: 12312,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "John",
+      nftsCount: 1111,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Steven",
+      nftsCount: 52,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Antonio Banderas",
+      nftsCount: 3,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Donald",
+      nftsCount: 12,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Peter",
+      nftsCount: 12312,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "John",
+      nftsCount: 1111,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Steven",
+      nftsCount: 52,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Antonio Banderas",
+      nftsCount: 3,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Donald",
+      nftsCount: 12,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Antonio Banderas",
+      nftsCount: 3,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+    {
+      name: "Donald",
+      nftsCount: 12,
+      avatar: "/images/avatar.png",
+      verified: true,
+    },
+  ]}/>
       <Footer/>
     </div>
   );
