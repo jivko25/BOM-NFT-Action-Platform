1337c8f9ddeca88f000610a8fed3b0576e9de3e0
jivko25 <jivkou@yahoo.com>
2021-11-08 23:57:42 +0200

    dummy fix 3

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -1,11 +1,9 @@
-import styles from './ProductImage.module.scss';
+import styles from "./ProductImage.module.scss"
 
-export default function ProductImage({url = ''}){
-    return(
-        <div>
-        <div className={"product-image"}>
-            <img src={`${url}`} className={styles.image} />
+export default function ProductImage({url}){
+    return (
+        <div className={styles["product-image"]}>
+            <img className={styles["image"]} src={url}/>
         </div>
-        </div>
-    );
+    )
 }
