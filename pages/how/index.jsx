1edbec9122c02f8358c2b46bdd1c21fb9e59f7dd
jivko25 <jivkou@yahoo.com>
2021-11-11 15:58:29 +0200

    add how page

---
+++ pages/how/index.jsx
@@ -0,0 +1,17 @@
+import Footer from "../../src/components/footer/Footer";
+import Header from "../../src/components/header/Header";
+import Hero from "../../src/components/hero/Hero";
+import Description from "../../src/components/description/Description";
+
+
+export default function How(){
+    return(
+    <>
+        <Header/>
+        <Hero text="Lorem Ipsum 123"/>
+        <Description text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+        image="https://nft-auction.herokuapp.com/uploads/0xa6dbe6b4f8e2905c26e123ec6fd08a8f7200dbc1_64120a76f4.jpg"/>
+        <Footer/>
+    </>
+    );
+}
