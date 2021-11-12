f762007b508239a635e56ed68f008599168bf4e2
jivko25 <jivkou@yahoo.com>
2021-11-12 16:03:54 +0200

    add ProfileCollectionFilters component

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -2,8 +2,10 @@ import Header from '../../src/components/header/Header';
 import Footer from '../../src/components/footer/Footer';
 import ProfileHero from '../../src/components/profile/ProfileHero';
 import ProfileUser from '../../src/components/profile/ProfileUser';
+import ProfileCollectionFilters from '../../src/components/profile/ProfileCollectionFilters';
 
 export default function Profile(){
+    
     return(
     <>
     <Header/>
@@ -13,6 +15,28 @@ export default function Profile(){
     avatar="https://nft-auction.herokuapp.com/uploads/0xa6dbe6b4f8e2905c26e123ec6fd08a8f7200dbc1_64120a76f4.jpg"
     verified={true}
     info="What are they they are the broken promise and to do is out of reach. Hand over the candy notes food is so expensive to the Cashier."
+    />
+    <ProfileCollectionFilters 
+        filters = {{
+            sort: [
+              { label: "Name (Ascending)", value: 1 },
+              { label: "Name (Descending)", value: 2 },
+              { label: "Price (Ascending)", value: 4 },
+              { label: "Price (Descending)", value: 5 },
+            ],
+            price: [
+              { label: "0.3 - 0.5 ETH", value: 6 },
+              {
+                label: "0.5 - 2 ETH",
+                value: 7,
+              },
+              {
+                label: "2- 3 ETH",
+                value: 8,
+              },
+            ],
+          }
+      }
     />
 
     <Footer/>
---
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { Grid, TextField, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import styles from './ProfileCollectionFilters.module.scss';
+
+export default function ProfileCollectionFilters({filters}){
+    const [sortBy, setSortBy] = useState('');
+    const [range, setRange] = useState('');
+
+    const handleChangeSort = (event) => {
+        setSortBy(event.target.value);
+    };
+
+    const handleChangeRange = (event) => {
+        setRange(event.target.value);
+    };
+
+    return(
+        <div className={styles["profile-collection-filters"]}>
+            <Grid container spacing={2}>
+                <Grid item xs={5}>
+                    <Typography variant={'h2'}>Collection</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    {/* Sort by */}
+                    <Box>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+                            <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={sortBy}
+                            label="Sort by"
+                            onChange={handleChangeSort}
+                            variant={'outlined'}
+                            color={'primary'}
+                            >
+                            {
+                                filters.sort.map((item) => {
+                                    return <MenuItem value={item.value}>{item.label}</MenuItem>
+                                })
+                            }
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Grid>
+                <Grid item xs={2}>
+                    {/* Price range */}
+                    <Box>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Price range</InputLabel>
+                            <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={range}
+                            label="Price range"
+                            onChange={handleChangeRange}
+                            variant={'outlined'}
+                            color={'primary'}
+                            >
+                            {
+                                filters.price.map((item) => {
+                                    return <MenuItem value={item.value}>{item.label}</MenuItem>
+                                })
+                            }
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Grid>
+                <Grid item xs={3}>
+                    {/* Search */}
+                    <Box>
+                        <TextField fullWidth id="fullWidth" variant={'standard'}/>
+                    </Box>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
