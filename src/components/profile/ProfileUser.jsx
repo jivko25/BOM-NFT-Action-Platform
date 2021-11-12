5ad708bc3218e993269077da47567e5cf7d6ad26
jivko25 <jivkou@yahoo.com>
2021-11-12 02:04:54 +0200

    dummy fix

--- src/components/profile/ProfileUser.jsx
+++ src/components/profile/ProfileUser.jsx
@@ -4,7 +4,7 @@ import { Typography } from '@mui/material';
 
 export default function ProfileUser({name = '', verified = false, avatar = '', info = ''}){
     return(
-        <div styles={styles["profile-user"]}>
+        <div className={styles["profile-user"]}>
             <Avatar url = {avatar} size={120} verified={verified} className={styles.image}/>
             <Typography variant={'h3'} className={styles.name}>
                 {name}
