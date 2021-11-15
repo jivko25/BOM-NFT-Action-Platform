2f0f462a8d19d3b35262549bcc32f2fb3655dd8b
jivko25 <jivkou@yahoo.com>
2021-11-15 15:41:40 +0200

    dummy fix

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,7 +1,8 @@
-import { Grid, Link } from '@mui/material';
+import { Grid } from '@mui/material';
 import styles from './ActivityListItem.module.scss';
 import Avatar from '../avatar/Avatar';
 import { formatDistance, parseISO } from 'date-fns';
+import Link from '../link/Link';
 // import { Link } from "react-router-dom";
 
 export default function ActivityListItem({created_at, user, nft, type = "like"}){
@@ -13,12 +14,12 @@ export default function ActivityListItem({created_at, user, nft, type = "like"})
                 </Grid>
                 <Grid item xs={10}>
                     <p>{nft.owner.username} {type}  
-                    &ensp;"
-                    <Link>
-                    {nft.name}
+                    &ensp;
+                    <Link href="/">
+                    "{nft.name}"
                     </Link>
-                    "&ensp;by&ensp;
-                     <Link>
+                    &ensp;by&ensp;
+                     <Link href="/">
                      {user.name}
                      </Link>
                      </p>
@@ -33,9 +34,9 @@ export default function ActivityListItem({created_at, user, nft, type = "like"})
                      </p>
                 </Grid>
             </Grid>
-            <Link to="/">{nft.name}{user.name}</Link>
-            <Link to="/">{nft.name}</Link>
-            <Link to="/">{user.name}</Link>
+            <Link href="/">{nft.name}{user.name}</Link>
+            <Link href="/">{nft.name}</Link>
+            <Link href="/">{user.name}</Link>
             <link href=''></link>
             <a href=''>test</a>
         </div>
