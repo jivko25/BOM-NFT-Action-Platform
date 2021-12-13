cb0392d237168ab5054a87265b1b68c7af59337f
jivko25 <jivkou@yahoo.com>
2021-12-13 23:51:42 +0200

    Fix feature component
    Fix problem with links

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,6 +1,7 @@
 import { Container, ImageList, ImageListItem } from "@mui/material";
 import { useRouter } from "next/router";
 import styles from "./Featured.module.scss"
+import Link from 'next/link'
 
 
 export default function Featured({items = []}){
@@ -21,13 +22,14 @@ export default function Featured({items = []}){
                     gap={20}
                 >
                 {items.map(item => (
+                        <Link href={`/product/${item.id}`} key={item.id}>
                         <ImageListItem key={item.image} cols={item.cols || 1} rows={item.rows || 1} id={item.id}>
                             <img 
                                 className={styles.image}
                                 src={item.image}
-                                onClick={()=>{changeRoute(`http://localhost:3000/product/${item.id}`)}}
                             />
                         </ImageListItem>
+                        </Link>
                 ))}
                 </ImageList>
             </Container>
